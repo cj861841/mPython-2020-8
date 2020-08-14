b901eaab3f018776adc0215cb87042d35586dc90
cj861841 <69449894+cj861841@users.noreply.github.com>
2020-08-14 15:11:30 +0800

    Add files via upload

---
+++ plantTree.py
@@ -0,0 +1,9 @@
+from mcpi.minecraft import Minecraft
+mc=Minecraft.create()
+x,y,z=mc.player.getPos()
+def plantTree(x,y,z):
+    mc.setBlocks(x+1,y+5,z+1,x-1,y+3,z-1,18)
+    mc.setBlocks(x,y,z,x,y+4,z,17)
+for t in range(0,50,3):
+    for i in range(0,50,3):
+        plantTree(x+i,y,z+t)
